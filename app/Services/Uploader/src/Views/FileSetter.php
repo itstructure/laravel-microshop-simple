71b16e2f783b3609515abd76e0781db2bba095d7
Andrey Girnik <girnikandrey@gmail.com>
2023-04-09 09:48:14 +0600

    get input id

--- app/Services/Uploader/src/Views/FileSetter.php
+++ app/Services/Uploader/src/Views/FileSetter.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FileSetter
 {
+    public static $counter = 0;
+
+    public static $autoInputIdPrefix = 'w';
+
+
+    /************************* CONFIG ATTRIBUTES *************************/
     /**
      * @var Model
      */
@@ -21,6 +27,16 @@ class FileSetter
      */
     private $value;
 
+    /**
+     * @var string|int
+     */
+    private $inputId;
+
+
+    /************************* PROCESS ATTRIBUTES *************************/
+
+
+    /********************** PROCESS PUBLIC METHODS ***********************/
     /**
      * @param array $config
      * @return FileSetter
@@ -52,10 +68,13 @@ class FileSetter
             'attribute' => $this->attribute,
             'value' => !empty($this->model)
                 ? $this->model->{$this->attribute}
-                : $this->value
+                : $this->value,
+            'inputId' => $this->getInputId()
         ])->render();
     }
 
+
+    /************************* CONFIG SETTERS *****************************/
     /**
      * @param Model $model
      * @return FileSetter
@@ -84,5 +103,44 @@ class FileSetter
     {
         $this->value = $value;
         return $this;
+    }
+
+    /**
+     * @param $value
+     * @return FileSetter
+     */
+    public function setInputId($value): self
+    {
+        $this->inputId = $value;
+        return $this;
+    }
+
+
+    /********************** PROCESS INTERNAL METHODS *********************/
+    private function getInputId()
+    {
+        if (empty($this->inputId)) {
+            $this->inputId = !empty($this->model)
+                ? $this->getInputIdByModel()
+                : $this->getInputIdGenerated();
+        }
+        return $this->inputId;
+    }
+
+    /**
+     * @return string
+     */
+    private function getInputIdByModel(): string
+    {
+        return $this->model->getTable() . '-' . $this->attribute
+        . (!empty($this->model->getKey()) ? '-' . $this->model->getKey() : '');
+    }
+
+    /**
+     * @return string
+     */
+    private function getInputIdGenerated(): string
+    {
+        return static::$autoInputIdPrefix . static::$counter++;
     }
 }
